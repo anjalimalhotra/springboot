98c747583413ac2b69f6c5da498c3e38ad21808a
root <root@ip-10-0-0-44.ap-south-1.compute.internal>
2018-09-30 20:35:50 +0000

    updtae

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -5,25 +5,24 @@ sudo service anup-routing stop
 
 # create anup-routing service
 cat > /etc/systemd/system/anup-routing.service <<'EOF'
-
-
 [Unit]
 Description=My Webapp Java REST Service
+After=network.target
+
 [Service]
-User=ubuntu
-# The configuration file application.properties should be here:
-#change this to your workspace
-WorkingDirectory=/home/ubuntu/deploy
+Type=forking
+User=root
 #path to executable. 
 #executable is a bash script which calls jar file
-ExecStart=/home/ubuntu/deploy/anup-routing
+ExecStart=/home/ubuntu/deploy/anup-routing.sh
+ExecStop=/home/ubuntu/deploy/anup-routing.sh
+
 SuccessExitStatus=143
 TimeoutStopSec=10
 Restart=on-failure
 RestartSec=5
 [Install]
 WantedBy=multi-user.target
-
 
 EOF
 
@@ -33,10 +32,11 @@ rm -rf /home/ubuntu/deploy
 
 # create directory deploy
 mkdir -p /home/ubuntu/deploy 
-
-cat > /home/ubuntu/deploy/anup-routing <<'EOF'
+mkdir -p /home/ubuntu/deploy/logs
+cat > /home/ubuntu/deploy/anup-routing.sh <<'EOF'
 #!/bin/sh
 sudo /usr/bin/java -jar /home/ubuntu/deploy/spring-boot-web-0.0.1-SNAPSHOT.jar  >> /home/ubuntu/deploy/logs/routing.log 2>&1
 
 EOF
 
+chmod 775 /home/ubuntu/deploy/anup-routing.sh 
